f799de06e713a2709c84064415e07978499a37b6
sehyunnnn <kkshyun56@gmail.com>
2025-06-30 17:12:00 +0900

    init : 프로젝트 초기 설정

---
+++ src/test/java/com/example/dotdot/DotdotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dotdot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DotdotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
