e2e06a4db95e3a24ca384e39a9c97b821e1b5fdd
김세현 <kkshyun56@gmail.com>
2025-09-03 13:42:53 +0900

    fix : 시간대 변경

--- src/main/java/com/example/dotdot/DotdotApplication.java
+++ src/main/java/com/example/dotdot/DotdotApplication.java
@@ -1,12 +1,20 @@
 package com.example.dotdot;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+import java.util.TimeZone;
+
 @EnableAsync
 @SpringBootApplication
 public class DotdotApplication {
+
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DotdotApplication.class, args);
